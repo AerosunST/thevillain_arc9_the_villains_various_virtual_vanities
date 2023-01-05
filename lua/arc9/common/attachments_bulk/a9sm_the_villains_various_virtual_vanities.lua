5021d3e2e91f0cdc74b453c32f629448db50a23f
Aerosun <mister541thatguy@gmail.com>
2023-01-04 18:03:06 -0600

    fixed a file oopsie.
    mb mb.

---
+++ lua/arc9/common/attachments_bulk/a9sm_the_villains_various_virtual_vanities.lua
@@ -0,0 +1,315 @@
+local ATT = {}
+
+ATT = {}
+
+ATT.PrintName = "ArcCW Logo"
+ATT.CompactName = "ArcCW"
+ATT.Description = [[The ArcCW bird you all know and love!
+Design by The Villain. (Formerly known as Aerosun.)]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/arccwlogo_1")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/Logos"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/arccwlogo_1"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_arccwlogo")
+ATT = {}
+
+ATT.PrintName = "Bad Dewds Logo"
+ATT.CompactName = "Bad Dewds"
+ATT.Description = [[ARE YOU A BAD ENOUGH DEWD TO POOP IN THE ENEMY'S SOUP? (Perhaps, even, cream in their coffee?)
+Design by The Villain.]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/baddewdslogo_1")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/Logos"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/baddewdslogo_1"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_baddewdslogo")
+ATT = {}
+
+ATT.PrintName = "Carl"
+ATT.CompactName = "Carl"
+ATT.Description = [[This is a dinosaur. He hates people who go out to eat 30 minutes before the restaurant closes. (His name is Carl.)
+"Design" by The Villain.]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/carl")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/Shitpost"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/carl"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_carl")
+ATT = {}
+
+ATT.PrintName = "Nicolas Cage"
+ATT.CompactName = "Nic Cage"
+ATT.Description = [[Do you have a moment to talk about The One True Lord, Nicolas Cage?]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/nicolascage")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/Shitpost"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/nicolascage"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_nicolascage")
+ATT = {}
+
+ATT.PrintName = "GPPA Bronze"
+ATT.CompactName = "GPPA Bronze"
+ATT.Description = [[In the Golden Pappa's Peepers Awards, bronze indicates noteworthiness. It's definitely a cut above the average post, because it was enough to remind us that GPPA is a thing.]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/gppabronze")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/GPPA"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/gppabronze"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_gppabronze")
+ATT = {}
+
+ATT.PrintName = "GPPA Diamond"
+ATT.CompactName = "GPPA Diamond"
+ATT.Description = [[In the Golden Pappa's Peepers Awards, diamond indicates the highest possible praise one could ever receive. This is the mark of unparalleled excellence. Few are worthy of a title as grand as this. What few do possess it are immortalized in the GPPA Diamond Hall of Fame.]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/gppadiamond")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/GPPA"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/gppadiamond"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_gppadiamond")
+ATT = {}
+
+ATT.PrintName = "GPPA Gold"
+ATT.CompactName = "GPPA Gold"
+ATT.Description = [[In the Golden Pappa's Peepers Awards, gold indicates excellence. Only the finest of posts may receive this award. It is a guarantee that anything bearing its mark is of excellent quality.]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/gppagold")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/GPPA"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/gppagold"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_gppagold")
+ATT = {}
+
+ATT.PrintName = "GPPA Silver"
+ATT.CompactName = "GPPA Silver"
+ATT.Description = [[In the Golden Pappa's Peepers Awards, silver indicates quality. While it may not be gold, it is still great and worthy of coming back to.]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/gppasilver")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/GPPA"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/gppasilver"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_gppasilver")
+ATT = {}
+
+ATT.PrintName = "Murvivi Arrowclub"
+ATT.CompactName = "Murvivi"
+ATT.Description = [[Chaotic little purple man. Likes to collect gear and with that collection create manmade horrors beyond any of our comprehension. He runs his own shop if you were interested in buying any of his odd contraptions.]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/murviviarrowclub")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/ROBLOX"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/murviviarrowclub"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_murviviarrowclub")
+ATT = {}
+
+ATT.PrintName = "New Diddlandia (Emblem)"
+ATT.CompactName = "ND (Emblem)"
+ATT.Description = [[New Diddlandia is a fictional country that was created as a means to escape from the alarmingly fast growing number of "Gaybabies" in Old Diddlandia. While they are very friendly people, they are also extremely hostile to anyone who opposes them. Femboys are STRICTLY forbidden from New Diddlandia and will be shot on sight.
+Design by The Villain.]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/newdiddlandiaemblem")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/Country"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/newdiddlandiaemblem"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_newdiddlandiaemblem")
+ATT = {}
+
+ATT.PrintName = "New Diddlandia (Flag)"
+ATT.CompactName = "ND (Flag)"
+ATT.Description = [[New Diddlandia is a fictional country that was created as a means to escape from the alarmingly fast growing number of "Gaybabies" in Old Diddlandia. While they are very friendly people, they are also extremely hostile to anyone who opposes them. Femboys are STRICTLY forbidden from New Diddlandia and will be shot on sight.
+Design by The Villain.]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/newdiddlandiaflag")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/Country"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/newdiddlandiaflag"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_newdiddlandiaflag")
+ATT = {}
+
+ATT.PrintName = "No Anime Allowed"
+ATT.CompactName = "No Anime"
+ATT.Description = [[Originally made for a game called "The Streets" on ROBLOX, it has occurred to me that you people may want this. Arctic made a tool that lets you make stickers en masse and that's just asking for weeaboos to spam low-quality shit with it.
+"Design" by The Villain.]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/noanimeallowed")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/Shitpost"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/noanimeallowed"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_noanimeallowed")
+ATT = {}
+
+ATT.PrintName = "Pappa's Peepers"
+ATT.CompactName = "Peepers"
+ATT.Description = [[Little is known about Pappa's Peepers aside from two things: He is both highly intelligent and naturally high-explosive. His diet consists solely of Pepperoni Pizza... Technically that's three things but... shut up!!]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/pappaspeepers")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/ROBLOX"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/pappaspeepers"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_pappaspeepers")
+ATT = {}
+
+ATT.PrintName = "StarSolar (Emblem)"
+ATT.CompactName = "Solar (Emblem)"
+ATT.Description = [[StarSolar is a fictional country that was created after the fall of New Diddlandia. After a failed war on femboys, and a quite brutal hostile takeover, New Diddlandia had two options: Surrender or give them the slip and move elsewhere. StarSolar gets its name from the method of which New Diddlandia used to sneak away, space travel.
+Design by The Villain.]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/starsolaremblem")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/Country"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/starsolaremblem"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_starsolaremblem")
+ATT = {}
+
+ATT.PrintName = "StarSolar (Flag)"
+ATT.CompactName = "Solar (Flag)"
+ATT.Description = [[StarSolar is a fictional country that was created after the fall of New Diddlandia. After a failed war on femboys, and a quite brutal hostile takeover, New Diddlandia had two options: Surrender or give them the slip and move elsewhere. StarSolar gets its name from the method of which New Diddlandia used to sneak away, space travel.
+Design by The Villain.]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/starsolarflag")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/Country"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/starsolarflag"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_starsolarflag")
+ATT = {}
+
+ATT.PrintName = "Tom Aero Commemorative Sticker"
+ATT.CompactName = "Tom Aero"
+ATT.Description = [[Remember that funny voice pack The Villain made in 2021? There's a sequel coming out in 2023! Now featuring a PLAYERMODEL and MANY more voice lines to prevent sound fatigue! Isn't that EXCITING!?
+Design by The Villain.]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/tomaerocommemorativesticker")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/Artwork"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/tomaerocommemorativesticker"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_tomaerocommemorativesticker")
+ATT = {}
+
+ATT.PrintName = "Vampire Hunter Society (Patriam)"
+ATT.CompactName = "VHS (Patriam)"
+ATT.Description = [[Est. 2073. Near-future military branch for a like-Earth planet named Patriam. They specialize in handling vampires and vampire-sympathizing terrorism. Much like the logo, Patriam is foreign to this dimension. If you happen to come across this insignia on any firearm or device, please be wary of any potential Void Leaks in your area.
+Design by The Villain.]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/vampirehuntersocietypatriam")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/Logos"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/vampirehuntersocietypatriam"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_vampirehuntersocietypatriam")
+ATT = {}
+
+ATT.PrintName = "Vampire Hunter Society (Scarlet Hunter)"
+ATT.CompactName = "VHS (Scarlet Hunter)"
+ATT.Description = [[The ORIGINAL Vampire Hunter Society. Although this is the 2018 version, it debuted in 2016 with REMILIA HUNTING SIMULATOR 2016. With Vampire Hunter Society also came with the concept of The Void. Both of these are a direct inspiration for their equivalents of the same name in Patriam. It is important to note that while they are the direct inspiration, they have no relationship whatsoever. Scarlet Hunter's portrayal of The Void is much more abstract (it's more symbolic if anything) than it is compared to Patriam's version. This logo is here purely for nostalgia.
+Design by The Villain.]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/vampirehuntersocietyscarlethunter")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/Logos"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/vampirehuntersocietyscarlethunter"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_vampirehuntersocietyscarlethunter")
+ATT = {}
+
+ATT.PrintName = "Vampirjager X Logo"
+ATT.CompactName = "VJX Logo"
+ATT.Description = [[Contrary to the name, you don't hunt that many vampires in Vampirjager X. Instead, it's a game where your enemy is up to YOUR definition of evil. You (and your friends) star in an alliance called ''The Shattered Apollo''. It is a multidimensional alliance. Your objective? Avert evil. It's very fast paced. Very arcade... And... I need someone to help me make it a real gamemode. Look, here's a demo video!! https://www.youtube.com/watch?v=J5XzPDg4v9I
+Design by The Villain.]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/vampirjagerxlogo")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/Logos"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/vampirjagerxlogo"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_vampirjagerxlogo")
+ATT = {}
+
+ATT.PrintName = "The Villain's Card (First Edition)"
+ATT.CompactName = "The Villain"
+ATT.Description = [[The Ace of Spades means bad luck. The Ace of Spades also means new beginnings.
+Design by The Villain.]]
+ATT.Icon = Material("stickers/the_villains_various_virtual_vanities/thevillainscardfirstedition")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "The Villain/Cards"
+
+ATT.StickerMaterial = "stickers/the_villains_various_virtual_vanities/thevillainscardfirstedition"
+
+ARC9.LoadAttachment(ATT, "sticker_the_villains_various_virtual_vanities_thevillainscardfirstedition")
